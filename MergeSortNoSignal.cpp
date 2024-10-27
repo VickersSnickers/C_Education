f7dd91ca36488ed32cc580b24a2768dea492504c
VickersSnickers <trojanhorsecard@gmail.com>
2024-10-27 16:36:18 +0300

    modified:   MergeSortNoSignal.cpp

--- MergeSortNoSignal.cpp
+++ MergeSortNoSignal.cpp
@@ -28,38 +28,29 @@ void Merge(int A[], int p, int q, int r){   // p - firstElemIndex, q - divider, 
     for (int i = 0; i < n2; ++i){
         R[i] = A[q + i + 1];
     }
-    int i = 0;
-    int j = 0;
+    int i = 0, j = 0, k = p;
 
-    for (int k = p; k <= r; ++k){
+    while (i < n1 && j < n2){
         if (L[i] < R[j]) {
             A[k] = L[i];
             ++i;
-
-            if (i == n1){
-                ++k;
-                for (j; j < n2;){
-                    A[k] = R[j];
-                    ++j;
-                    ++k;
-                }
-            }
-
         } else {
             A[k] = R[j];
             ++j;
-
-            if (j == n2){
-                ++k;
-                for (i; i < n1;){
-                    A[k] = L[i];
-                    ++i;
-                    ++k;
-                }
-            }
-
         }
+        ++k;
     }
+    while (i < n1){
+        A[k] = L[i];
+        ++i;
+        ++k;
+    }
+    while (j < n2){
+        A[k] = R[j];
+        ++j;
+        ++k;
+    }
+    
 }
 
 void Merge_Sort(int A[], int p, int r){
